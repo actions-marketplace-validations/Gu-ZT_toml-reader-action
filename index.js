999f0472a14228f81bb3580cffd5d240f70f8dcb
Gugle <administration@ztxy666.cn>
2026-05-25 01:24:52 +0800

    add initial implementation of TOML reader action with type definitions and configuration

---
+++ index.js
@@ -0,0 +1,37 @@
+import * as core from '@actions/core';
+import {parse} from 'smol-toml';
+import * as fs from 'fs';
+
+function readField(parsedDoc, field) {
+    const path = field.split('.');
+    let value = parsedDoc;
+    path.forEach((f) => {
+        value = value[f];
+    });
+    return {path, value};
+}
+
+try {
+    const file = core.getInput('file');
+    const fields = core.getMultilineInput('fields');
+    const value = {};
+    const doc = fs.readFileSync(file, {encoding: 'utf8'});
+    const parsedDoc = parse(doc);
+    fields.forEach((field) => {
+        const {path, value: fieldValue} = readField(parsedDoc, field);
+        let current = value;
+        path.forEach((f, i) => {
+            if (i === path.length - 1) {
+                current[f] = fieldValue;
+            } else {
+                if (!current[f]) {
+                    current[f] = {};
+                }
+                current = current[f];
+            }
+        });
+    });
+    core.setOutput('value', value);
+} catch (error) {
+    core.setFailed(error.message);
+}
